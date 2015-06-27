d37a88907c441c456df05bb5a02bd7fc125d134e
Simon Fenton <swfenton@gmail.com>
2015-06-27 21:08:07 +1000

    Init

---
+++ src/PactNet.FSharp/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("PactNet.FSharp")>]
+[<assembly: AssemblyProductAttribute("PactNet.FSharp")>]
+[<assembly: AssemblyDescriptionAttribute("A functional wrapper for making PactNet nicer to work with from FSharp")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
